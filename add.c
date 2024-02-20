2cbbc957e5a177173aefc8577c51752daf1858b1
Khushi-3003 <156066200+Khushi-3003@users.noreply.github.com>
2024-02-20 14:17:22 +0530

    Update add.c

--- add.c
+++ add.c
@@ -3,13 +3,13 @@
 int main() {
     int num1, num2, sum;
     
-    printf("Enter the first number: ");
+    printf("Enter the 1st number: ");
     scanf("%d", &num1);
     
-    printf("Enter the second number: ");
+    printf("Enter the 2nd number: ");
     scanf("%d", &num2);
 
-    printf("Enter the third number: ");
+    printf("Enter the 3rd number: ");
     scanf("%d", &num3);
     
     sum = num1 + num2 + num3;
